c11f3092ed3ddec7cee29b125c71caf9a771cd97
nanspro <rahulbishnoi3636@gmail.com>
2020-08-09 18:31:44 +0530

    Relevant CLI commands

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ import shutil
 import maya
 import traceback
 import msgpack
+import ipfshttpclient
+
 
 from timeit import default_timer as timer
 from umbral.keys import UmbralPublicKey, UmbralPrivateKey
@@ -18,12 +20,6 @@ from nucypher.crypto.kits import UmbralMessageKit
 from nucypher.crypto.powers import DecryptingPower, SigningPower
 from nucypher.network.middleware import RestMiddleware
 from nucypher.datastore.keypairs import DecryptingKeypair, SigningKeypair
-# from nucypher.utilities.logging import GlobalLoggerSettings
-
-# from listener_keys import get_listener_pubkeys
-
-# Twisted Logger
-# GlobalLoggerSettings.start_console_logging()
 
 class nuCLI(object):
     """A simple nuCLI class having all nuCypher capabilities"""
@@ -39,9 +35,12 @@ class nuCLI(object):
         self.passphrase = "TEST_ALICIA_INSECURE_DEVELOPMENT_PASSWORD"
         self.PUBLIC_JSON = 'public.json'
         self.PRIVATE_JSON = 'private.json'
-    
-    def generate_keys(self):
-        '''Generate key pair'''
+
+    def initialize(self, email, restore=False):
+        '''Initializing stuff, generating key pair'''
+        # Creating ipfs api object
+        self.api = ipfshttpclient.connect('/dns/localhost/tcp/5001/http')
+
         enc_privkey = UmbralPrivateKey.gen_key()
         sig_privkey = UmbralPrivateKey.gen_key()
 
@@ -61,51 +60,9 @@ class nuCLI(object):
         }
         with open(self.PUBLIC_JSON, 'w') as f:
             json.dump(pubkeys, f)
-
-    # put it inside get_privkeys
-    def _get_keys(self, file, key_class):
-        if not os.path.isfile(file):
-            self.generate_keys()
-
-        with open(file) as f:
-            stored_keys = json.load(f)
-        keys = dict()
-        for key_type, key_str in stored_keys.items():
-            keys[key_type] = key_class.from_bytes(bytes.fromhex(key_str))
-        return keys
-
-
-    def get_privkeys(self):
-        '''Get private keys of actor bob'''
-        return self._get_keys(self.PRIVATE_JSON, UmbralPrivateKey)
-
-
-    def get_alice(self):
-        '''Get Alice actor'''
-        ursula = Ursula.from_seed_and_stake_info(seed_uri=self.SEEDNODE_URI,
-                                            federated_only=True,
-                                            minimum_stake=0)
-        # A new Alice is restored from the configuration file
-        new_alice_config = AliceConfiguration.from_configuration_file(
-            filepath=os.path.join(self.TEMP_ALICE_DIR, 'alice.config.json'),
-            domains={TEMPORARY_DOMAIN},
-            known_nodes={ursula},
-            start_learning_now=False,
-            federated_only=True,
-            learn_on_same_thread=True,
-        )
-
-        # Alice unlocks her restored keyring from disk
-        new_alice_config.attach_keyring()
-        new_alice_config.keyring.unlock(password=self.passphrase)
-        new_alice = new_alice_config()
-        new_alice.start_learning_loop(now=True)
-
-        return new_alice
-
-
-    def initialize_alice(self):
-        '''Initialzing Alice'''
+        
+        ############################
+        # Initializing Alice
         ursula = Ursula.from_seed_and_stake_info(seed_uri=self.SEEDNODE_URI,
                                             federated_only=True,
                                             minimum_stake=0)
@@ -136,7 +93,45 @@ class nuCLI(object):
         
         # Let's get to learn about the NuCypher network
         alicia.start_learning_loop(now=True)
-        return alicia, alice_config_file
+
+        ##############################
+        # Initializing Bob
+
+        # Getting private keys
+        with open(self.PRIVATE_JSON) as f:
+            stored_keys = json.load(f)
+        bob_privkeys = dict()
+        for key_type, key_str in stored_keys.items():
+            bob_privkeys[key_type] = UmbralPrivateKey.from_bytes(bytes.fromhex(key_str))
+
+        ursula = Ursula.from_seed_and_stake_info(seed_uri=self.SEEDNODE_URI,
+                                            federated_only=True,
+                                            minimum_stake=0)
+
+        shutil.rmtree(self.TEMP_BOB_DIR, ignore_errors=True)
+
+        bob_enc_keypair = DecryptingKeypair(private_key=bob_privkeys["enc"])
+        bob_sig_keypair = SigningKeypair(private_key=bob_privkeys["sig"])
+        enc_power = DecryptingPower(keypair=bob_enc_keypair)
+        sig_power = SigningPower(keypair=bob_sig_keypair)
+        power_ups = [enc_power, sig_power]
+
+        print("Creating Bob ...")
+
+        bob = Bob(
+            domains={TEMPORARY_DOMAIN},
+            federated_only=True,
+            crypto_power_ups=power_ups,
+            start_learning_now=True,
+            abort_on_learning_error=True,
+            known_nodes=[ursula],
+            save_metadata=False,
+            network_middleware=RestMiddleware(),
+        )
+        print("Bob = ", bob)
+
+        self.alice = alicia
+        self.bob = bob
 
 
     def get_policy_pubkey(self, label):
@@ -151,12 +146,67 @@ class nuCLI(object):
             #   "label '{}' is {}".format(label.decode("utf-8"), policy_pubkey.to_bytes().hex()))
         return policy_pubkey
 
+    # Not needed probably
+    def get_alice(self):
+        '''Get Alice actor'''
+        ursula = Ursula.from_seed_and_stake_info(seed_uri=self.SEEDNODE_URI,
+                                            federated_only=True,
+                                            minimum_stake=0)
+        # A new Alice is restored from the configuration file
+        new_alice_config = AliceConfiguration.from_configuration_file(
+            filepath=os.path.join(self.TEMP_ALICE_DIR, 'alice.config.json'),
+            domains={TEMPORARY_DOMAIN},
+            known_nodes={ursula},
+            start_learning_now=False,
+            federated_only=True,
+            learn_on_same_thread=True,
+        )
 
-    def grant_access_policy(self, label, bob_pubkeys):
+        # Alice unlocks her restored keyring from disk
+        new_alice_config.attach_keyring()
+        new_alice_config.keyring.unlock(password=self.passphrase)
+        new_alice = new_alice_config()
+        new_alice.start_learning_loop(now=True)
+
+        return new_alice
+
+    def encrypt(self, label, file_path):
+        '''Encrypting a full folder with files and uploading it to ipfs'''
+        policy_pubkey = self.get_policy_pubkey(label)
+        data_source = Enrico(policy_encrypting_key=policy_pubkey)
+        data_source_public_key = bytes(data_source.stamp)
+        print(file_path)
+
+        with open(file_path, "rb") as f:
+            plaintext = f.read()
+
+        ciphertext, signature = data_source.encrypt_message(plaintext)
+        
+        print("Signature", signature)
+        data = {
+            'image_data': ciphertext.to_bytes(),
+            'data_source': data_source_public_key
+        }
+        
+        with open(file_path + '_encrypted', "wb") as f:
+            msgpack.dump(data, f, use_bin_type=True)
+        
+        enc_file = file_path + '_encrypted'
+        ipfsHash = self.api.add(enc_file)
+        ipfsHash = ipfsHash['Hash']
+        
+        return ipfsHash
+
+
+    def share(self, label, bob_pubkeys, email, ipfsHash):
         '''
         Alicia creates a policy granting access to Bob.
-        The policy is sent to the NuCypher network.
+        The policy is sent to the NuCypher network and bob recieves an email with policy info.
         '''
+
+        # TODO: Get bob_pubkeys from email
+        # TODO: Send email with policydata and ipfsHash
+
         alicia = self.get_alice()
         label = label.encode()
         # We create a view of the Bob who's going to be granted access.
@@ -181,61 +231,21 @@ class nuCLI(object):
             "label": label.decode(),
         }))
 
-    def initialize_bob(self):
-        '''Initialzing Bob'''
-        bob_privkeys = self.get_privkeys()
-        # instead of passing priv keys, we should pass file and read keys from there
-        ursula = Ursula.from_seed_and_stake_info(seed_uri=self.SEEDNODE_URI,
-                                            federated_only=True,
-                                            minimum_stake=0)
 
-        # Remove previous demo files and create new ones
-        shutil.rmtree(self.TEMP_BOB_DIR, ignore_errors=True)
-
-        bob_enc_keypair = DecryptingKeypair(private_key=bob_privkeys["enc"])
-        bob_sig_keypair = SigningKeypair(private_key=bob_privkeys["sig"])
-        enc_power = DecryptingPower(keypair=bob_enc_keypair)
-        sig_power = SigningPower(keypair=bob_sig_keypair)
-        power_ups = [enc_power, sig_power]
-
-        print("Creating the Buyer ...")
-
-        buyer = Bob(
-            domains={TEMPORARY_DOMAIN},
-            federated_only=True,
-            crypto_power_ups=power_ups,
-            start_learning_now=True,
-            abort_on_learning_error=True,
-            known_nodes=[ursula],
-            save_metadata=False,
-            network_middleware=RestMiddleware(),
-        )
-        self.bob = buyer
-
-        print("Buyer = ", buyer)
-        return buyer
-
-
-    def join_policy(self, policy_metadata): 
-        '''Join a policy created by someone else'''
-        # Let's join the policy generated by Alicia. We just need some info about it.
-
-        buyer = self.bob
+    def fetch(self, policy_metadata, ipfsHash): 
+        '''Fetches data from ipfs and then decrypts it'''
+        # Join Policy
+        bob = self.bob
         policy_pubkey = UmbralPublicKey.from_bytes(bytes.fromhex(policy_metadata["policy_pubkey"]))
         alices_sig_pubkey = UmbralPublicKey.from_bytes(bytes.fromhex(policy_metadata["alice_sig_pubkey"]))
         label = policy_metadata["label"].encode()
 
-        print("The Buyer joins policy for label '{}'".format(label.decode("utf-8")))
-        buyer.join_policy(label, alices_sig_pubkey)
-        return label.decode("utf-8")
-
-    def reencrypt_segment(self, enc_data, policy_metadata):
-        ''' Re-encrypt the data and then decrypt it'''
-        listener = self.bob
-        policy_pubkey = UmbralPublicKey.from_bytes(bytes.fromhex(policy_metadata["policy_pubkey"]))
-        alices_sig_pubkey = UmbralPublicKey.from_bytes(bytes.fromhex(policy_metadata["alice_sig_pubkey"]))
-        label = policy_metadata["label"].encode()
+        print("Joins policy for label '{}'".format(label.decode("utf-8")))
+        bob.join_policy(label, alices_sig_pubkey)
         
+        # Re-encrypt the data and then decrypt it
+        # Getting data from ipfs
+        enc_data = self.api.cat(ipfsHash)
         data = msgpack.loads(enc_data)
         message_kit = UmbralMessageKit.from_bytes((data[b'track_segment_data']))
         data_source = Enrico.from_public_keys(
@@ -245,7 +255,7 @@ class nuCLI(object):
         plaintext = None
         try:
             start = timer()
-            retrieved_plaintexts = listener.retrieve(
+            retrieved_plaintexts = bob.retrieve(
                 message_kit,
                 label=label,
                 enrico=data_source,
